8d7395d03b0ce80911ca437e8fa267a01c2ccdaf
gastonsantos <gastons525@gmail.com>
2024-11-25 14:29:37 -0300

    agrego filtros para cors

---
+++ src/main/java/com/api/wordToo/config/CustomCorsFilter.java
@@ -0,0 +1,35 @@
+package com.api.wordToo.config;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomCorsFilter implements Filter {
+
+    @Override
+    public void doFilter(jakarta.servlet.ServletRequest request, jakarta.servlet.ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        // Configuración de las cabeceras CORS
+        httpResponse.setHeader("Access-Control-Allow-Origin", "https://your-frontend.com");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpResponse.setHeader("Access-Control-Allow-Private-Network", "true");
+        httpResponse.setHeader("Access-Control-Max-Age", "7200");
+
+        // Manejo de solicitudes preflight (OPTIONS)
+        if ("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
+            httpResponse.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+}
